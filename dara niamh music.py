87eb1c068b353e19e1dde0fbbea2917832f2c875
Dara Cullen <15dcullen@stjosephsrush.com>
2017-05-18 15:31:32 +0100

    Add files via upload

---
+++ dara niamh music.py
@@ -0,0 +1,176 @@
+#This is Dara and Niamh's Spotify music helper
+#Coding 2nd Year 2017 - Alpha1
+import webbrowser #for later use.....
+from time import sleep
+music=input("Please choose a genre from this list. pop, rock, indie, rnb, jazz, metal ,punk, disco, , 80s ,90s, reggae, Disney, romance, musicals, classical ")
+
+
+if music == "pop":
+  print("Ciao Adios by Anne-Marie")
+  sleep(2)
+  print("The Cure by Lady GaGa")
+  sleep(2)
+  print("Symphony by Clean Bandit feat. Zara Larsson")
+  sleep(2)
+  print("Green Light by Lorde")
+  sleep(2)
+  print("Despacito-Remix by Luis Fonsi, Daddy Yankee, Justin Bieber")
+  sleep(2)
+  print("I'm the One by DJ Khaled, Justin Bieber")
+  sleep(2)
+  print("Swalla by Nicki Minaj and Ty Dolla $ign")
+  sleep(2)
+  print("Malibu by Miley Cyrus")
+   
+
+elif music == "rock":
+  print("We have a few songs that we can recommend to you.")
+  print("Seven Nation Army by The White Stripes")
+  sleep(2)
+  print("Should I Stay or Should I Go by The Clash")
+  sleep(2)
+  print("Come Together by The Beatles")
+  sleep(2)
+  print("We Will Rock You by Queen") #this piece was by dara
+  sleep(2)
+  print("Edge of Seventeen by Stevie Nicks")
+  
+  
+elif music == "Indie":
+  print("fluorescent Adolescent by Arctic Monkeys")
+  sleep(2)
+  print("Niave by The Kooks")
+  sleep(2)
+  print("Little Talks by Monsters Of Men")
+  sleep(2)
+  print("Are You Gonna Be My Girl by Jet")
+  sleep(2)
+  print("Pumped Up Kicks by Foster The People")
+  
+  
+elif music == "R&B":
+  print("I'm The One by DJ Khaled, Justin Bieber, Quavo, Chance The Rapper, Lil Wayne")
+  sleep(2)
+  print("Mask Off by Future")
+  sleep(2)
+  print("Passion Fruit by Drake")
+  sleep(2)
+  print("No Frauds by Nicki Minaj, Drake, Lil Wayne")
+  sleep(2)
+  print("Party by Chris Brown")
+  
+  
+elif music == "Jazz":
+  print("Dedicated To You by Freddie Hubbard")
+  sleep(2)
+  print("So What by Miles Davis")
+  sleep(2)
+  print("Blue Skies by Art Tatum")
+  sleep(2)
+  print("My Favourite Things by John Coltrane")
+  sleep(2)
+  print("Take Five by The Dave Brubeck Quartet")
+  
+  
+elif music == "metal":
+  print("Murder One by Of Metallica")
+  sleep(2)
+  print("The End by In Flames")
+  sleep(2)
+  print("Don't Need You by Bullet For My Valentine")
+  sleep(2)
+  print("Rotting In Vain by Korn")
+  sleep(2)
+  print("God Damn by Avenged Sevenfold")
+  
+elif music == "punk":
+  print("I have a great few songs for you.")
+  sleep(2)
+  print("Most of My Chemical Romance's songs are extremely great.")
+  sleep(2)
+  print("Don't Threaten Me With A Good Time by P!ATD")
+  sleep(2)
+  print("King For A Day by Bring Me The Horizon") # this was by dara
+  sleep(2)
+  print("Heavydirtysoul by Twenty One Pilots")
+    
+elif choice == "disco":
+  print("Here are the Top 5 disco songs.")
+  sleep(2)
+  print("Night Fever by Bees Gees")
+  sleep(2)
+  print("I Will Survive by Gloria Gaynor") #this was by dara
+  sleep(2)
+  print("We Are Family by Sister Sledge")
+  sleep(2)
+  print("Staying Alive by Bees Gees")
+  sleep(2)
+  print("Funkytown by Lipps Inc.")
+  
+
+  
+elif choice == "romance":
+  print("The Night We Met by Lord Huron")
+  sleep(2)
+  print("You Said You'd Grow Old With Me by Michael Schulte")
+  sleep(2)
+  print("Love Like This by Kodaline") #by dara
+  sleep(2)
+  print("Only You by Selena Gomez")
+  sleep(2)
+  print("Northern Downpour by P!ATD")
+  sleep(2)
+  print("Can't Help Falling In Love by Elvis")
+  sleep(2)
+  print("Halo by Beyonce")
+  sleep(2)
+  print("All of Me by John Legend")
+  sleep(2)
+  print("Because of You by Kelly Clarkson")
+  sleep(2)
+  print("The Power Of Love by Gabrielle Aplin")
+  
+elif choice == "musicals":
+  sleep(2)
+  print("Out Tonight by Rosario Dawson- Rent")
+  sleep(2)
+  print("Say No To This by Jasmine Cephas Jones, Leslie Odom Jr., Lin Manuel Miranda, Sydney James Harcourt, Original Broadway Cast Of Hamilton- Hamilton")
+  sleep(2)
+  print("Take Me Or Leave Me by Indina Menzel, Tracie Thoms-Rent")
+  sleep(2)
+  print("The Schuyler Sisters by Renee Elise Goldsberry, Phillipa Soo, Jasmine Cephas Jones, Leslie Odom JR., Original Broadway Cast Of Hamilton- Hamilton")
+  sleep(2)
+  print("Cell Block Tango by Catherine Zeta Jones, Susan Misner, Deidre Goodwin, Denise Faye, Ekaterina Chtchelkanova, Mya Harrison, Taye Diggs - Chicago")
+  sleep(2)
+  print("96000 by lin Manuel Miranda-In The Heights")
+  sleep(2)
+  print("Only Us by Ben Platt, Laura Dreyfuss- Dear Evan Hansen")
+  
+elif choice == "80's":
+  print("maniac by michael sembello")
+  sleep(2)
+  print("take on me by a-ha")
+  sleep(2)
+  print("sweet dreams by eurythmics")
+  sleep(2)
+  print("another one bites the dust by queen")
+  sleep(2)
+  print("eye of the tiger by survivor")
+    
+elif choice == "90's":
+  print("everybody by backstreet boys")
+  sleep(2)
+  print("Blue by eiffel 65")
+  sleep(2)
+  print("what is love by haddaway")
+  sleep(2)
+  print("say my name by destinys child")
+  sleep(2)
+  print("All star by smash mouth")
+  sleep(2)
+  print("the rhythm of the night by corona")
+  sleep(2)
+  print("Rhythm is a dancer by SNAP!")
+  sleep(2)
+  print("U can't touch this by MC Hammer")
+5
